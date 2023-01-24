9079590b77fe1cb2f24b10ab67af2d78fe75c14b
dylan <dylanfruit@outlook.fr>
2023-01-24 15:39:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const url = "https://geek-jokes.sameerkumar.website/api";
+
+const randomJokeResult = document.querySelector(".random-joke-result");
+const randomJokeBtn = document.querySelector(".random-joke-btn");
+window.onload = () => {
+  randomJokeResult.style.display = "none";
+};
+
+function fetchJokes() {
+  fetch(url)
+    .then((response) =>
+      response.json().then((data) => {
+        randomJokeResult.innerHTML = data;
+      })
+    )
+    .catch((error) => console.log(error));
+}
+
+console.log(fetchJokes());
+
+randomJokeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  randomJokeResult.style.display = "block";
+  fetchJokes();
+});
